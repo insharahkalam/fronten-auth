4329673756f8f4d1879f8c6b69ae2c506105c914
insharahkalam <insharahkalam19@gmail.com>
2026-05-06 04:56:17 +0500

    updated

--- src/config/service.js
+++ src/config/service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = 'http://localhost:3000/api/auth'
+const url = 'http://localhost:3000/api/auth' || 'https://fronten-auth.vercel.app/api/auth'
 
 const api = axios.create({
     baseURL: url,
