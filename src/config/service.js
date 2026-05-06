e5815f815b466f8c5f0f28d96f6ccfa5fc30ff44
insharahkalam <insharahkalam19@gmail.com>
2026-05-06 19:30:28 +0500

    again

--- src/config/service.js
+++ src/config/service.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const url =
     import.meta.env.MODE === "development"
         ? "http://localhost:3000/api/auth"
-        : "https://backend-authentication-lake.vercel.app//api/auth";
+        : "https://backend-authentication-lake.vercel.app/api/auth";
 
 const api = axios.create({
     baseURL: url,
