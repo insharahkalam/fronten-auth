e5b2a2cb58d793fa2ef9aa379b7f1a3256908ff3
insharahkalam <insharahkalam19@gmail.com>
2026-05-06 05:11:14 +0500

    updated again

--- src/config/service.js
+++ src/config/service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = 'http://localhost:3000/api/auth' || 'https://fronten-auth.vercel.app/'
+const url = 'http://localhost:3000/api/auth' || 'https://backend-authentication-wine.vercel.app/api/auth'
 
 const api = axios.create({
     baseURL: url,
